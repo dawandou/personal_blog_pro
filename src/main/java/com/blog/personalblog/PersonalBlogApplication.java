c80dfebeae963b7979905866b8c3bf0db4fb2532
tiger <2390472338@qq.com>
2021-10-13 23:33:19 +0800

    初始化项目

---
+++ src/main/java/com/blog/personalblog/PersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.personalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/personalblog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
